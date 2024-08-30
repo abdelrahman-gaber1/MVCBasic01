670a7d3651bc1129719ac61f25fa58b8e9330d1a
abdelrahman-gaber1 <abdogaber487@gmail.com>
2024-08-30 22:13:56 +0300

    version1.1

--- MVCBasic01/Controllers/MovieController.cs
+++ MVCBasic01/Controllers/MovieController.cs
@@ -9,13 +9,33 @@ namespace MVCBasic01.Controllers
     public class MovieController :Controller
     {
         // Action --> Moive/GetMovie --> Route for this function
-        public void GetMovie(int id)
+        // Action have special return type contentResult
+        // to avoid develop againest concreate class we use IActionResult
+        // IActionResult interface all special type of action inhert form him
+        //develop againest Interface not concreate class
+        public IActionResult GetMovie(int id)
         {
+            ContentResult result = new ContentResult();
+            result.Content = $"Movie with id = {id}";
+            result.ContentType = "object/pdf" ;
+            result.StatusCode = 200 ;
 
+            return Content($"Movie with id = {id}", "txt/html") ; //helper method only in controller
         }
-        public string Index()
+        // route became Movie/hamada insted of index
+        [ActionName("hamada")]
+        // to reach to this method http request must be Get
+        [HttpGet]
+        public IActionResult Index()
         {
-            return "Index";
+            RedirectResult redirectResult = new RedirectResult("https://localhost:44322/Movie/GetMovie");
+            //return Redirect("https://localhost:44322/Movie/GetMovie"); //helper method 
+            //return RedirectToAction(nameof(GetMovie), new {id = 10});//helper method
+            return RedirectToRoute("default"); 
+        }
+        public IActionResult Hamada()
+        {
+            return View();
         }
     }
 }
